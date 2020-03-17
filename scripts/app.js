d3809d543f37e2b7a20cb94d5e6d3f7214ff0760
Ellenstarbuck <ellenclarestarbuck@gmail.com>
2020-03-17 10:25:38 +0000

    work

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,24 @@
-import 'bulma'
-import './styles/main.scss'
-
 function init() {
 
+  const playButton = document.querySelector('#play-btn')
+  const stopButton = document.querySelector('#stop-btn')
+  console.log(playButton)
+  const audio = document.querySelector('audio')
   
-  
+  function playSound () {
+    console.log('play the sound now')
+    audio.src = 'sounds/strokes.mp3'
+    audio.play()
+  }
 
+  function stopSound () {
+    audio.pause()
+  }
+
+
+  //event listeners for the button
+  playButton.addEventListener('click', playSound)
+  stopButton.addEventListener('click', stopSound)
 }
 
 

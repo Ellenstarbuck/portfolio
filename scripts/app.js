b4ebccdb90585bf3b724543363e890485234c982
Ellenstarbuck <ellenclarestarbuck@gmail.com>
2020-03-23 17:35:27 +0000

    text slideshow done

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,43 @@
 function init() {
 
+  const previous = document.querySelector('.prev')
+  const next = document.querySelector('.next')
+
+  var slideIndex = 1
+
+  // Next/previous controls
+  
+
+  function nextSlide() {
+    showSlides(slideIndex += 1)
+    
+  }
+
+  function previousSlide() {
+    showSlides(slideIndex -= 1)
+  }
+
+
+  function showSlides(n) {
+    var i
+    var slides = document.querySelectorAll('.mySlides')
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none'
+    }
+    slides[slideIndex - 1].style.display = 'block'
+    
+  }
+
+  showSlides(slideIndex)
+
+  previous.addEventListener('click', previousSlide)
+  next.addEventListener('click', nextSlide)
   
 }
 
